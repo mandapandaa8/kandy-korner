1701618e128b0e0b1f23e44ff44412f6a7135432
Amanda De Leon <108186573+mandapandaa8@users.noreply.github.com>
2022-08-30 14:31:56 -0500

    Revert "all of the locations render in list plus css"

--- src/components/views/ApplicationViews.js
+++ src/components/views/ApplicationViews.js
@@ -1,23 +1,6 @@
-
-import { Outlet, Route, Routes } from "react-router-dom"
-import { LocationList } from "../Locations/LocationsList"
-import "./ApplicationViews.css"
-
 export const ApplicationViews = () => {
-	return (
-        <Routes>
-            <Route path="/" element={
-                <>
-                    <h1 className="header">Kandy Korner</h1>
-                    <div className="candy_fix">🍬🍬🍬🍬🍬Get your Kandy fix here🍬🍬🍬🍬🍬</div>
+	return <>
 
-                    <Outlet />
-                </>
-            }>
-
-                <Route path="locations" element={ <LocationList /> } />
-            </Route>
-        </Routes>
-    )
+	</>
 }
 
